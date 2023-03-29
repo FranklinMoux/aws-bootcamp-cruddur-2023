15fe7f230d190c8831be0f624a90047a6fc0c5b4
Franklin Moux <110202420+FranklinMoux@users.noreply.github.com>
2023-03-29 12:10:27 -1000

    Update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -181,7 +181,7 @@ def data_notifications():
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
-@xray_recorder.capture(''activities_users')
+@xray_recorder.capture('activities_users')
 def data_handle(handle):
   model = UserActivities.run(handle)
   if model['errors'] is not None:
