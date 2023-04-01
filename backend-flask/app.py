bba4e75d0b4ec10278d7cf94adb3cf3b246bcf87
Franklin Moux <110202420+FranklinMoux@users.noreply.github.com>
2023-03-31 14:10:42 -1000

    Update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -123,12 +123,16 @@ frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
 
-CORS(
-    app,
-    resources={r"/api/*": {"origins": origins}},
-    expose_headers="location,link",
-    allow_headers="content-type,if-modified-since, traceparent",
-    methods="OPTIONS,GET,HEAD,POST"
+app = Flask(__name__)
+frontend = os.getenv('FRONTEND_URL')
+backend = os.getenv('BACKEND_URL')
+origins = [frontend, backend]
+cors = CORS(
+  app, 
+  resources={r"/api/*": {"origins": origins}},
+  expose_headers="location,link",
+  allow_headers="content-type,if-modified-since",
+  methods="OPTIONS,GET,HEAD,POST"
 )
 
 from flask import Flask
