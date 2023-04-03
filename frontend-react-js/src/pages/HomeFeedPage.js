52b6a1052aa41a1d88e45fea66d8449f57a054d2
Franklin Moux <110202420+FranklinMoux@users.noreply.github.com>
2023-04-02 15:56:43 -1000

    Update HomeFeedPage.js

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,96 +1,82 @@
-import "./HomeFeedPage.css";
+import './MessageGroupPage.css';
 import React from "react";
+import { useParams } from 'react-router-dom';
 
-import DesktopNavigation from "../components/DesktopNavigation";
-import DesktopSidebar from "../components/DesktopSidebar";
-import ActivityFeed from "../components/ActivityFeed";
-import ActivityForm from "../components/ActivityForm";
-import ReplyForm from "../components/ReplyForm";
-import { useAuth } from "../store/authentication";
-import { Auth } from 'aws-amplify';
+import DesktopNavigation  from '../components/DesktopNavigation';
+import MessageGroupFeed from '../components/MessageGroupFeed';
+import MessagesFeed from '../components/MessageFeed';
+import MessagesForm from '../components/MessageForm';
+import checkAuth from '../lib/CheckAuth';
 
-export default function HomeFeedPage() {
-  const [activities, setActivities] = React.useState([]);
-  const [popped, setPopped] = React.useState(false);
-  const [poppedReply, setPoppedReply] = React.useState(false);
-  const [replyActivity, setReplyActivity] = React.useState({});
-  const { user, setUser, authToken } = useAuth();
+
+export default function MessageGroupPage() {
+  const [messageGroups, setMessageGroups] = React.useState([]);
+  const [messages, setMessages] = React.useState([]);
+  const [popped, setPopped] = React.useState([]);
+  const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
+  const params = useParams();
 
-  const loadData = async () => {
+  const loadMessageGroupsData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`;
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
       const res = await fetch(backend_url, {
-        method: "GET",
         headers: {
-          'Authorization': `Bearer ${authToken}`,
-          'Content-Type': 'application/json'
-        }
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
+        method: "GET"
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        setActivities(resJson);
+        setMessageGroups(resJson)
       } else {
-        console.log(res);
+        console.log(res)
       }
     } catch (err) {
       console.log(err);
     }
-  };
+  };  
 
-  const checkAuth = async () => {
-    Auth.currentAuthenticatedUser({
-      // Optional, By default is false. 
-      // If set to true, this call will send a 
-      // request to Cognito to get the latest user data
-      bypassCache: false
-    })
-      .then((user) => {
-        return Auth.currentAuthenticatedUser()
-      }).then((cognito_user) => {
-        setUser?.({
-          username: cognito_user.attributes.name,
-          handle: cognito_user.attributes.preferred_username
-        })
-      })
-      .catch((err) => {
-        console.log(err);
+  const loadMessageGroupData = async () => {
+    try {
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
+      const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
+        method: "GET"
       });
-  };
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessages(resJson)
+      } else {
+        console.log(res)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };  
 
-  React.useEffect(() => {
+
+  React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
-    loadData();
-    checkAuth();
-  }, []);
-
+    loadMessageGroupsData();
+    loadMessageGroupData();
+    checkAuth(setUser);
+  }, [])
   return (
     <article>
-      <DesktopNavigation user={user} active={"home"} setPopped={setPopped} />
-      <div className="content">
-        <ActivityForm
-          popped={popped}
-          setPopped={setPopped}
-          setActivities={setActivities}
-        />
-        <ReplyForm
-          activity={replyActivity}
-          popped={poppedReply}
-          setPopped={setPoppedReply}
-          setActivities={setActivities}
-          activities={activities}
-        />
-        <ActivityFeed
-          title="Home"
-          setReplyActivity={setReplyActivity}
-          setPopped={setPoppedReply}
-          activities={activities}
-        />
+      <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
+      <section className='message_groups'>
+        <MessageGroupFeed message_groups={messageGroups} />
+      </section>
+      <div className='content messages'>
+        <MessagesFeed messages={messages} />
+        <MessagesForm setMessages={setMessages} />
       </div>
-      <DesktopSidebar user={user} />
     </article>
   );
 }
