3d4930c12ccf9c59eb818279e3341c66c226a15f
Franklin Moux <110202420+FranklinMoux@users.noreply.github.com>
2023-04-02 16:00:34 -1000

    implement HomeFeedPage

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,25 +1,26 @@
-import './MessageGroupPage.css';
+import './HomeFeedPage.css';
 import React from "react";
-import { useParams } from 'react-router-dom';
+
 
 import DesktopNavigation  from '../components/DesktopNavigation';
-import MessageGroupFeed from '../components/MessageGroupFeed';
-import MessagesFeed from '../components/MessageFeed';
-import MessagesForm from '../components/MessageForm';
+import DesktopSidebar     from '../components/DesktopSidebar';
+import ActivityFeed from '../components/ActivityFeed';
+import ActivityForm from '../components/ActivityForm';
+import ReplyForm from '../components/ReplyForm';
 import checkAuth from '../lib/CheckAuth';
 
 
-export default function MessageGroupPage() {
-  const [messageGroups, setMessageGroups] = React.useState([]);
-  const [messages, setMessages] = React.useState([]);
-  const [popped, setPopped] = React.useState([]);
+export default function HomeFeedPage() {
+  const [activities, setActivities] = React.useState([]);
+  const [popped, setPopped] = React.useState(false);
+  const [poppedReply, setPoppedReply] = React.useState(false);
+  const [replyActivity, setReplyActivity] = React.useState({});
   const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
-  const params = useParams();
 
-  const loadMessageGroupsData = async () => {
+  const loadData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`
@@ -28,55 +29,56 @@ export default function MessageGroupPage() {
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        setMessageGroups(resJson)
+        setActivities(resJson)
       } else {
         console.log(res)
       }
     } catch (err) {
       console.log(err);
     }
-  };  
+  };
 
-  const loadMessageGroupData = async () => {
-    try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
-      const res = await fetch(backend_url, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
-        },
-        method: "GET"
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setMessages(resJson)
-      } else {
-        console.log(res)
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };  
 
+// check when the page loads if we are authenicated
+React.useEffect(()=>{
+  loadData();
+  checkAuth(setUser);
+}, [])
 
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
-    loadMessageGroupsData();
-    loadMessageGroupData();
-    checkAuth(setUser);
+    loadData();
+    checkAuth();
   }, [])
+
   return (
     <article>
       <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
-      <section className='message_groups'>
-        <MessageGroupFeed message_groups={messageGroups} />
-      </section>
-      <div className='content messages'>
-        <MessagesFeed messages={messages} />
-        <MessagesForm setMessages={setMessages} />
+      <div className='content'>
+        <ActivityForm
+          user_handle={user} 
+          popped={popped}
+          setPopped={setPopped} 
+          setActivities={setActivities} 
+        />
+        <ReplyForm 
+          activity={replyActivity} 
+          popped={poppedReply} 
+          setPopped={setPoppedReply} 
+          setActivities={setActivities} 
+          activities={activities} 
+        />
+        <ActivityFeed 
+          title="Home" 
+          setReplyActivity={setReplyActivity} 
+          setPopped={setPoppedReply} 
+          activities={activities} 
+        />
       </div>
+      <DesktopSidebar user={user} />
     </article>
   );
 }
