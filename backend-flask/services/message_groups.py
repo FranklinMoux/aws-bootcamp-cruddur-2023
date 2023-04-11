662cca46e0fa47c73653890e41cf3aba6dbfb12e
Franklin Moux <110202420+FranklinMoux@users.noreply.github.com>
2023-04-11 09:19:29 -1000

    Update message_groups.py

--- backend-flask/services/message_groups.py
+++ backend-flask/services/message_groups.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta, timezone
 
 from lib.ddb import Ddb
 from lib.db import db
-#from lib.momento import MomentoCounter
 
 class MessageGroups:
   def run(cognito_user_id):
@@ -20,8 +19,7 @@ class MessageGroups:
 
     ddb = Ddb.client()
     data = Ddb.list_message_groups(ddb, my_user_uuid)
-    print("list_message_groups: ",data)
+    print("list_message_groups",data)
 
-    #MomentoCounter.reset(f"msgs/{user_handle}")
     model['data'] = data
     return model
